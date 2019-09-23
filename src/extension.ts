a4fbac138590c183ab409edd07f10821df09b072
Salvatore <salvo@salvo-desk.inf.ethz.ch>
2019-09-23 15:24:02 +0200

    improve code + add translation cache

--- src/extension.ts
+++ src/extension.ts
@@ -3,34 +3,139 @@
 import * as vscode from 'vscode';
 import { POINT_CONVERSION_COMPRESSED } from 'constants';
 
+type TranlationCache = { [label: string]: SourceCodeLine };
 
 const trace_regex = "[ ]+[0-9]+[ ]+([0-9]+)[ ]+(1c[0-9a-f]+).+";
 
+
+const open_options: vscode.OpenDialogOptions = {
+	canSelectMany: false,
+	canSelectFolders: false,
+	filters: { 'All files': ['*']}
+};
+
+// create a decorator type that we use to decorate large numbers
+const source_code_line = vscode.window.createTextEditorDecorationType({
+	cursor: 'crosshair',
+	backgroundColor: '#FF000055',
+});
+
 class SourceCodeLine {
-	path:string;
-	line:number;
-	cycles:number;
-	pc:string;
+	workspace:vscode.WorkspaceFolder;
+	filepath:string;
+	linenumber:number;
+	binfile_path:string;
 
-	constructor(txt:string){
-		let matches = txt.match(trace_regex);
-		if (matches===null || matches.length<1) {return; }
+	constructor(workspace:vscode.WorkspaceFolder, filepath:string, linenumber:number, binfile_path:string){
+		this.workspace = workspace;
+		this.filepath = filepath;
+		this.linenumber = linenumber;
+		this.binfile_path = binfile_path;
+	}
 
-		let cycles=matches[1];
-		let pc=matches[2];
+	isInWorkspace(){
+		return this.filepath.indexOf(this.workspace.name) >= 0;
+	}
+
+	getURI() {
+		
+		if (!this.isInWorkspace()) { return undefined; }
+		let source_file = this.filepath.substring(this.filepath.indexOf(this.workspace.name) + this.workspace.name.length + 1);
+		return vscode.Uri.parse("file://" + this.workspace.uri.fsPath + "/" + source_file);
 	}
 }
 
+class TraceLine {
+	cycles:number;
+	pc:string;
+
+	constructor(txt:string, regex:string){
+		this.cycles = 0;
+		this.pc = "0x0";
+		let matches = txt.match(regex);
+		if (matches===null || matches.length<1) {return; }
+
+		this.cycles=parseInt(matches[1]);
+		this.pc=matches[2];
+	}
+
+	async resolve(trace_translation_cache:TranlationCache, workspace:vscode.WorkspaceFolder, binfile:string) {
+		return new Promise<SourceCodeLine>((resolve, reject) => {
+
+			if (trace_translation_cache[this.pc] !== undefined) { resolve(trace_translation_cache[this.pc]);}
+
+			const cp = require("child_process");
+
+			cp.exec("addr2line -e " + binfile + " -i " + this.pc, (error: string, stdout: string, stderr: any) => {
+				if (error) {
+					reject(stderr);
+				}
+				
+				const stdout_lines = stdout.split("\n");
+				//const stdout_line = stdout_lines[stdout_lines.length -2];
+				const stdout_line = stdout_lines[0];
+				const coords = stdout_line.split(":");
+				const filepath = coords[0];
+				const line = parseInt(coords[1]);
+				trace_translation_cache[this.pc] = new SourceCodeLine(workspace, filepath, line, binfile);
+				resolve(trace_translation_cache[this.pc]);
+			});
+		});
+	}
+
+}
+
+function highlightLines(text_editor:vscode.TextEditor, line_start:number, line_stop:number, line_select:number, select:boolean, recenter:boolean, decs:vscode.DecorationOptions []){
+
+	for (let line=line_start; line<=line_stop; line++) {
+		let dec = {range: text_editor.document.lineAt(line).range};
+		decs.push(dec);
+	}
+	text_editor.setDecorations(source_code_line, decs);
+
+	if (select) {text_editor.selection = new vscode.Selection(line_select, 0, line_select, 0);}
+	const first_visible_line = text_editor.visibleRanges[0].start.line;
+	const last_visible_line = text_editor.visibleRanges[0].end.line;
+	//console.log("line_select " + line_select + "; range: " + first_visible_line + " -> " + last_visible_line);
+	if (recenter && (line_select < first_visible_line || line_select > last_visible_line)) { text_editor.revealRange(text_editor.selection, vscode.TextEditorRevealType.InCenter); }
+
+}
+
+function highlightTrace(trace_translation_cache:TranlationCache, trace_editor:vscode.TextEditor, ws:vscode.WorkspaceFolder, binfile_uri:string, source:SourceCodeLine){
+/*	
+	trace_editor.visibleRanges.forEach( range => {
+		let trace_line_start = range.start.line;
+		let trace_line_stop = range.end.line;
+		
+		let decs:vscode.DecorationOptions [] = [];
+		for (let i=trace_line_start; i<=trace_line_stop; i++){
+			let trace_line = new TraceLine(trace_editor.document.lineAt(i).text, trace_regex);
+			trace_line.resolve(ws, binfile_uri).then( (other_source) => {
+				if (other_source.filepath === source.filepath) {
+					highlightLine(trace_editor, i, false, false, decs);
+				}
+			});
+		}
+	});
+*/
+	let decs:vscode.DecorationOptions [] = [];
+	//highlightLines(trace_editor, 0, trace_editor.document.lineCount-1, 0, false, false, decs);
+
+	trace_editor.document.getText().split("\n").forEach( (line, index) => {
+		let trace_line = new TraceLine(trace_editor.document.lineAt(index).text, trace_regex);
+		trace_line.resolve(trace_translation_cache, ws, binfile_uri).then( (other_source) => {
+			if (other_source.filepath === source.filepath) {
+				highlightLines(trace_editor, index, index, index, false, false, decs);
+			}
+		});
+	});
+
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// create a decorator type that we use to decorate large numbers
-	const source_code_line = vscode.window.createTextEditorDecorationType({
-		cursor: 'crosshair',
-		backgroundColor: '#FF000055',
-	});
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -49,102 +154,55 @@ export function activate(context: vscode.ExtensionContext) {
 	let trace_editor = (<vscode.TextEditor>vscode.window.activeTextEditor);
 	let trace_document = (<vscode.TextEditor>vscode.window.activeTextEditor).document;
 	let binfile_uri = "";
-	let source_editor:vscode.TextEditor;
 
-	let wsname = (<vscode.WorkspaceFolder[]>vscode.workspace.workspaceFolders)[0].name;	
-	let wspath = (<vscode.WorkspaceFolder[]>vscode.workspace.workspaceFolders)[0].uri.fsPath;	
+	let ws = (<vscode.WorkspaceFolder[]>vscode.workspace.workspaceFolders)[0];	
 
-	/*vscode.workspace.openTextDocument().then(doc => {
-		vscode.window.showTextDocument(doc, vscode.ViewColumn.Beside).then(editor => {
-			source_editor = editor;
-		});
-	});*/
+	let current_source_code_line:SourceCodeLine;
 
-	const open_options: vscode.OpenDialogOptions = {
-		canSelectMany: false,
-		canSelectFolders: false,
-		filters: { 'All files': ['*']}
-	};
-	
 	vscode.window.showOpenDialog(open_options).then(fileUri => {
 		if (fileUri && fileUri[0]) {
 			binfile_uri = fileUri[0].fsPath;
 		}
 	});
 
-	vscode.window.onDidChangeTextEditorVisibleRanges(changeEvent => {
+	let trace_translation_cache:TranlationCache = {};
+	/*vscode.window.onDidChangeTextEditorVisibleRanges(changeEvent => {
 
-		if (changeEvent.textEditor.document !== trace_document) {return;}
+		if (changeEvent.textEditor.document !== trace_document || current_source_code_line === undefined) {return;}
+		highlightTrace(trace_editor, ws, binfile_uri, current_source_code_line);
 
-		changeEvent.visibleRanges.forEach( range => {
-			console.log(range);
-		});
-
-	});
+	});*/
 
 	vscode.window.onDidChangeTextEditorSelection(changeEvent => {
 
-		if (vscode.window.activeTextEditor === undefined || (<vscode.TextEditor>vscode.window.activeTextEditor).document !== trace_document) { return; }
+		if (vscode.window.activeTextEditor === undefined || changeEvent.textEditor.document !== trace_document) { return; }
 
 		let sel = changeEvent.textEditor.selection;
 		if (!sel.isSingleLine) {return;}
-		let line_num = sel.start.line;
 		let line_txt = changeEvent.textEditor.document.lineAt(sel.start.line).text;
 
-		let matches = line_txt.match("[ ]+[0-9]+[ ]+([0-9]+)[ ]+(1c[0-9a-f]+).+");
-		if (matches===null || matches.length<1) {return; }
+		let selected_trace_line = new TraceLine(line_txt, trace_regex);
 
-		let cycles=matches[1];
-		let pc=matches[2];
+		selected_trace_line.resolve(trace_translation_cache, ws, binfile_uri).then( (source) => {
 
-		console.log(cycles);
-		console.log(pc);
-
-
-		const cp = require("child_process");
-
-		
-		cp.exec("addr2line -e " + binfile_uri + " " + pc.toString(), (error: string, stdout: string, stderr: any) => {
-			if (error) {
-				console.error("exec error: " + error);
-			}
-			let coords = stdout.split(":");
-
-			if (coords[0].indexOf(wsname) >= 0) {
-				let filename = coords[0];
-				let line = parseInt(coords[1]);
-	
-				let source_file = coords[0].substring(filename.indexOf(wsname) + wsname.length + 1);
-				let source_uri = vscode.Uri.parse("file://" + wspath + "/" + source_file);
+			if (source.isInWorkspace()) {
+				console.log(source.filepath);
+				console.log(source.linenumber);
+				let source_uri = <vscode.Uri>source.getURI();
 				console.log("opening: " + source_uri.fsPath);
-				console.log("wspath: " + wspath);
-				console.log("line: " + line);
+				current_source_code_line = source;
 				vscode.workspace.openTextDocument(source_uri).then(doc => { 
 					vscode.window.showTextDocument(doc, vscode.ViewColumn.Beside, true).then( source_editor => {
-						source_editor.selection = new vscode.Selection(line, 0, line, 0);
-						let decs:vscode.DecorationOptions[] = [];
-						source_editor.revealRange(source_editor.selection, vscode.TextEditorRevealType.InCenter);
-						let dec = {range: source_editor.document.lineAt(line).range};
-						decs.push(dec);
-						source_editor.setDecorations(source_code_line, decs);
-
-						trace_editor.visibleRanges.forEach( range => {
-							let trace_line_start = range.start.line;
-							let trace_line_stop = range.end.line;
-
-							for (let i=trace_line_start; i<=trace_line_stop; i++){
-								
-							}
-
-						});
-
-					}); 
+						highlightLines(source_editor, source.linenumber, source.linenumber, source.linenumber, true, true, []);
+						//highlightLine(source_editor, source.linenumber, true, true, []);
+						highlightTrace(trace_translation_cache, trace_editor, ws, binfile_uri, source);
+					});
 				});
+			} 
+			else {
+				console.log("The file: " + source.filepath + " (line " + source.linenumber +") is not in the current workspace! (raw: " + line_txt +")");
 			}
 		});
-
-
-
 	});
 	
 
