539305db54cf584c7dd16f912ffe6b43e7e7ca14
Salvatore <salvo@salvo-desk.inf.ethz.ch>
2019-09-26 10:10:36 +0200

    reloading trace every time the command is executed

--- src/extension.ts
+++ src/extension.ts
@@ -10,8 +10,8 @@ import { TLSSocket } from 'tls';
 type PCIndex = { [label: string]: SourceLine };
 type TranlationIndex = { [label: number]: TraceLine };
 type SourceLineCache = { [label: string]: SourceLine };
-type FileIndex = { [label: string]: TraceLine []};
-type FileDecIndex = { [label: string]: vscode.Range []};
+type FileIndex = { [label: string]: TraceLine[] };
+type FileDecIndex = { [label: string]: vscode.Range[] };
 
 
 
@@ -21,7 +21,7 @@ const trace_regex = "[ ]+[0-9]+[ ]+([0-9]+)[ ]+(1c[0-9a-f]+).+";
 const open_options: vscode.OpenDialogOptions = {
 	canSelectMany: false,
 	canSelectFolders: false,
-	filters: { 'All files': ['*']}
+	filters: { 'All files': ['*'] }
 };
 
 // create a decorator type that we use to decorate large numbers
@@ -33,11 +33,11 @@ const source_code_line = vscode.window.createTextEditorDecorationType({
 });
 
 class SourceLine {
-	path:string;
-	line:number;
-	uri:string;
+	path: string;
+	line: number;
+	uri: string;
 
-	constructor(path:string, line:number, uri:string){
+	constructor(path: string, line: number, uri: string) {
 		this.path = path;
 		this.line = line;
 		this.uri = uri;
@@ -45,13 +45,13 @@ class SourceLine {
 }
 
 class TraceLine {
-	cycles:number;
-	pc:string;
-	trace_line_num:number;
-	trace_line_length:number;
-	source_lines:SourceLine[];
-	
-	constructor(pc:string, cycles:number, trace_line_num:number, trace_line_length:number){
+	cycles: number;
+	pc: string;
+	trace_line_num: number;
+	trace_line_length: number;
+	source_lines: SourceLine[];
+
+	constructor(pc: string, cycles: number, trace_line_num: number, trace_line_length: number) {
 		this.pc = pc;
 		this.cycles = cycles;
 		this.source_lines = [];
@@ -60,18 +60,33 @@ class TraceLine {
 	}
 }
 
+class Addr2LineView {
+	trace_editor: vscode.TextEditor;
+
+	constructor(trace_editor: vscode.TextEditor) {
+		this.trace_editor = trace_editor;
+	}
+
+	update(trace_editor: vscode.TextEditor) {
+		this.trace_editor = trace_editor;
+	}
+
+	getTraceDocument() {
+		return (<vscode.TextEditor>vscode.window.activeTextEditor).document;
+	}
+}
 
 class Addr2Line {
-	workspace:vscode.WorkspaceFolder;
-	binfile_path:string;
-	regex:string;
-	translation_index:TranlationIndex;
-	source_line_objs:SourceLineCache;
-	pcindex:PCIndex;
-	fileindex:FileIndex;
-	filedecindex:FileDecIndex;
+	workspace: vscode.WorkspaceFolder;
+	binfile_path: string;
+	regex: string;
+	translation_index: TranlationIndex;
+	source_line_objs: SourceLineCache;
+	pcindex: PCIndex;
+	fileindex: FileIndex;
+	filedecindex: FileDecIndex;
 
-	constructor(workspace:vscode.WorkspaceFolder, binfile_path:string, regex:string){
+	constructor(workspace: vscode.WorkspaceFolder, binfile_path: string, regex: string) {
 		this.workspace = workspace;
 		this.binfile_path = binfile_path;
 		this.regex = regex;
@@ -82,21 +97,21 @@ class Addr2Line {
 		this.filedecindex = {};
 	}
 
-	async translate_lines(trace_lines:string[]){
+	async translate_lines(trace_lines: string[]) {
 
-		return new Promise( async (resolve, reject) => {
+		return new Promise(async (resolve, reject) => {
 
 			const slice = 1000;
-			
+
 			let promises = [];
-			for (let i=0; i<trace_lines.length; i+=slice){
-			
+			for (let i = 0; i < trace_lines.length; i += slice) {
+
 				//console.log("i: " + i + " trace_lines.length: " + trace_lines.length);
-				let trace_lines_obj = trace_lines.slice(i, i + slice - 1).map( (line, index) => { 
+				let trace_lines_obj = trace_lines.slice(i, i + slice - 1).map((line, index) => {
 					let matches = line.match(this.regex);
-					if (matches===null || matches.length<1) {return new TraceLine("0", 0, 0, 0);}
-					const cycles=parseInt(matches[1]);
-					const pc=matches[2];
+					if (matches === null || matches.length < 1) { return new TraceLine("0", 0, 0, 0); }
+					const cycles = parseInt(matches[1]);
+					const pc = matches[2];
 					return new TraceLine(pc, cycles, i + index, line.length);
 				});
 
@@ -106,7 +121,7 @@ class Addr2Line {
 
 				const cp = require("child_process");
 
-				
+
 
 				await new Promise((resolve, reject) => {
 					cp.exec("addr2line -e " + this.binfile_path + " -i -a " + program_counters_txt, (error: string, stdout: string, stderr: any) => {
@@ -118,21 +133,21 @@ class Addr2Line {
 						const stdout_lines = stdout.split("\n");
 						//console.log(stdout_lines.length);
 						let trace_obj_index = -1;
-						for (let i=0; i<stdout_lines.length; i++){
+						for (let i = 0; i < stdout_lines.length; i++) {
 							let stdout_line = stdout_lines[i];
 							//console.log(stdout_line);
 							if (stdout_line.startsWith("0x")) {
 								trace_obj_index++;
-							} 
+							}
 							else {
-								if (trace_obj_index<0) { throw Error("add2line output format is not recognized!"); }
+								if (trace_obj_index < 0) { throw Error("add2line output format is not recognized!"); }
 
 								let tl = trace_lines_obj[trace_obj_index];
 
 								//the line has been translated and is the file is in our workspace (well....)
-								if (!stdout_line.startsWith("??") && stdout_line.indexOf(this.workspace.name) >= 0){
-									
-									if (this.source_line_objs[stdout_line] === undefined){
+								if (!stdout_line.startsWith("??") && stdout_line.indexOf(this.workspace.name) >= 0) {
+
+									if (this.source_line_objs[stdout_line] === undefined) {
 										let coords = stdout_line.split(":");
 										const source_path = coords[0];
 										const source_line = parseInt(coords[1]);
@@ -149,17 +164,17 @@ class Addr2Line {
 									const file_idx_key = this.source_line_objs[stdout_line].path;
 									if (this.fileindex[file_idx_key] === undefined) {
 										this.fileindex[file_idx_key] = [tl];
-										this.filedecindex[file_idx_key] = [new vscode.Range(tl.trace_line_num, 0, tl.trace_line_num, tl.trace_line_length-1)];
+										this.filedecindex[file_idx_key] = [new vscode.Range(tl.trace_line_num, 0, tl.trace_line_num, tl.trace_line_length - 1)];
 										//let dec = {range: trace_editor.document.lineAt(trace_line.trace_line_num).range};
 
-									}else { 
-										this.fileindex[file_idx_key].push(tl); 
-										this.filedecindex[file_idx_key].push(new vscode.Range(tl.trace_line_num, 0, tl.trace_line_num, tl.trace_line_length-1));
+									} else {
+										this.fileindex[file_idx_key].push(tl);
+										this.filedecindex[file_idx_key].push(new vscode.Range(tl.trace_line_num, 0, tl.trace_line_num, tl.trace_line_length - 1));
 									}
 								}
 							}
 						}
-						console.log("completed batch " + i + " -> " + (i+slice));
+						console.log("completed batch " + i + " -> " + (i + slice));
 						//progress.report("Line: " )
 						resolve();
 					});
@@ -167,7 +182,7 @@ class Addr2Line {
 				});
 
 				//trace_lines_obj.map((obj) => { console.log("trace line: " + obj.trace_line_num + "; sources: " + obj.source_lines.length); });
-				trace_lines_obj.map((obj) => { if (obj.source_lines.length>0) { this.translation_index[obj.trace_line_num] = obj;}});
+				trace_lines_obj.map((obj) => { if (obj.source_lines.length > 0) { this.translation_index[obj.trace_line_num] = obj; } });
 			}
 			console.log("completed: " + Object.keys(this.translation_index).length);
 			resolve();
@@ -175,21 +190,21 @@ class Addr2Line {
 	}
 }
 
-function highlightLines(text_editor:vscode.TextEditor, line_start:number, line_stop:number, line_select:number, select:boolean, recenter:boolean, decs:vscode.DecorationOptions []){
+function highlightLines(text_editor: vscode.TextEditor, line_start: number, line_stop: number, line_select: number, select: boolean, recenter: boolean, decs: vscode.DecorationOptions[]) {
 
-	for (let line=line_start; line<=line_stop; line++) {
-		let dec = {range: text_editor.document.lineAt(line).range};
+	for (let line = line_start; line <= line_stop; line++) {
+		let dec = { range: text_editor.document.lineAt(line).range };
 		decs.push(dec);
 	}
 	text_editor.setDecorations(source_code_line, decs);
 
-	if (select) {text_editor.selection = new vscode.Selection(line_select, 0, line_select, 0);}
+	if (select) { text_editor.selection = new vscode.Selection(line_select, 0, line_select, 0); }
 	const first_visible_line = text_editor.visibleRanges[0].start.line;
 	const last_visible_line = text_editor.visibleRanges[0].end.line;
 	//console.log("line_select " + line_select + "; range: " + first_visible_line + " -> " + last_visible_line);
 	if (recenter && (line_select < first_visible_line || line_select > last_visible_line)) { text_editor.revealRange(text_editor.selection, vscode.TextEditorRevealType.InCenter); }
-
 }
+
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -200,57 +215,29 @@ export async function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "trace2source" is now active!');
 
-	if ( vscode.window.activeTextEditor === undefined){
+	if (vscode.window.activeTextEditor === undefined) {
 		vscode.window.showErrorMessage("Need to focus on the trace window before executing this command!");
 		return;
 	}
 
-	if (vscode.workspace.workspaceFolders === undefined){
+	if (vscode.workspace.workspaceFolders === undefined) {
 		vscode.window.showErrorMessage("Need to have a workspace open!");
 		return;
 	}
 
 	let trace_editor = (<vscode.TextEditor>vscode.window.activeTextEditor);
-	let trace_document = (<vscode.TextEditor>vscode.window.activeTextEditor).document;
-	let binfile_uri = "";
+	let ws = (<vscode.WorkspaceFolder[]>vscode.workspace.workspaceFolders)[0];
 
-	let ws = (<vscode.WorkspaceFolder[]>vscode.workspace.workspaceFolders)[0];	
+	let addr2line = new Addr2Line(ws, "", trace_regex);
+	let addr2line_view = new Addr2LineView(trace_editor);
 
-
-	let trace_lines = trace_document.getText().split("\n");
-
-
-	await vscode.window.showOpenDialog(open_options).then(fileUri => {
-		if (fileUri && fileUri[0]) {
-			binfile_uri = fileUri[0].fsPath;
-		}
-	});
-
-	let addr2line = new Addr2Line(ws, binfile_uri, trace_regex);
-	
-
-	await vscode.window.withProgress(
-		{
-		  location: vscode.ProgressLocation.Notification,
-		  title: 'Processing trace file',
-		  cancellable: true
-		},
-		async progress => {
-							
-		
-			await addr2line.translate_lines(trace_document.getText().split("\n")).then(() => {
-				console.log("all lines have been translated!");
-			});
-			console.log(Object.keys(addr2line.translation_index).length);
-		}
-	);
 
 	vscode.window.onDidChangeTextEditorSelection(changeEvent => {
 
-		if (vscode.window.activeTextEditor === undefined || changeEvent.textEditor.document !== trace_document) { return; }
+		if (vscode.window.activeTextEditor === undefined || changeEvent.textEditor.document !== addr2line_view.getTraceDocument()) { return; }
 
 		let sel = changeEvent.textEditor.selection;
-		if (!sel.isSingleLine) {return;}
+		if (!sel.isSingleLine) { return; }
 
 		const sel_line = sel.start.line;
 
@@ -258,15 +245,15 @@ export async function activate(context: vscode.ExtensionContext) {
 			const tline = addr2line.translation_index[sel_line];
 			const sline = addr2line.translation_index[sel_line].source_lines[0];
 			let source_uri = sline.uri;
-			vscode.workspace.openTextDocument(source_uri).then(doc => { 
-				vscode.window.showTextDocument(doc, vscode.ViewColumn.Beside, true).then( source_editor => {
+			vscode.workspace.openTextDocument(source_uri).then(doc => {
+				vscode.window.showTextDocument(doc, vscode.ViewColumn.Beside, true).then(source_editor => {
 					console.log("highlighting line " + sline.line + " of file " + sline.path);
 					highlightLines(source_editor, sline.line, sline.line, sline.line, true, true, []);
 
 					let trace_lines_dec = addr2line.filedecindex[sline.path];
-					if (trace_lines_dec !== undefined){
+					if (trace_lines_dec !== undefined) {
 
-						trace_editor.setDecorations(source_code_line, trace_lines_dec);
+						addr2line_view.trace_editor.setDecorations(source_code_line, trace_lines_dec);
 						console.log("highlighting " + trace_lines_dec.length + " trace lines");
 					}
 				});
@@ -275,16 +262,41 @@ export async function activate(context: vscode.ExtensionContext) {
 		else {
 			console.log("No translation for line " + sel_line + "!");
 		}
-
 	});
-	
+
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('extension.examineTrace', async () => {
 		// The code you place here will be executed every time your command is executed
-	
+
+		let trace_editor = (<vscode.TextEditor>vscode.window.activeTextEditor);
+		let ws = (<vscode.WorkspaceFolder[]>vscode.workspace.workspaceFolders)[0];
+
+		addr2line.workspace = ws;
+		addr2line_view.trace_editor = trace_editor;
+
+		await vscode.window.showOpenDialog(open_options).then(fileUri => {
+			if (fileUri && fileUri[0]) {
+				addr2line.binfile_path = fileUri[0].fsPath;
+			}
+		});
+
+		await vscode.window.withProgress(
+			{
+				location: vscode.ProgressLocation.Notification,
+				title: 'Processing trace file',
+				cancellable: true
+			},
+			async progress => {
+				await addr2line.translate_lines(addr2line_view.getTraceDocument().getText().split("\n")).then(() => {
+					console.log("all lines have been translated!");
+				});
+				console.log(Object.keys(addr2line.translation_index).length);
+			}
+		);
+
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Trace processing complete!');
 
@@ -295,4 +307,4 @@ export async function activate(context: vscode.ExtensionContext) {
 
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
